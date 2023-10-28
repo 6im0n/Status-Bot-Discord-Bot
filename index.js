90742ed56b8b9e9a1cfc03013a104072f10d4637
Debian <freebox@freebox-vm-14934>
2023-10-28 02:29:29 +0000

    [add] change the server embeded test for jellyfin server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const axios = require('axios');
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, EmbedBuilder  } = require('discord.js');
 
 const channel_status_id = process.env.CHANEL_ID;
 const serverUrl = process.env.URL_TO_PING;
@@ -17,27 +17,58 @@ const client = new Client({
   ]
 });
 
+const serverDownEmbeded = new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle('Le serveur à arrêté de fonctionner 🤯')
+	.setAuthor({ name: 'Serveur non disponible', iconURL: 'https://github.com/jellyfin/jellyfin-ux/blob/master/branding/tizen/icon.png?raw=true', url: 'https://discord.js.org' })
+	.setThumbnail('https://github.com/jellyfin/jellyfin-ux/blob/master/branding/tizen/icon.png?raw=true')
+	// .addFields(
+	// 	{ name: 'Regular field title', value: 'Some value here' },
+	// 	{ name: '\u200B', value: '\u200B' },
+	// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// )
+	.setImage('https://media.giphy.com/media/WpaVhEcp3Qo2TjwyI1/giphy.gif')
+	.setTimestamp()
+
+  const serverUPEmbeded = new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle('Le serveur fonctionne de nouveau ✨')
+	.setAuthor({ name: 'Serveur actif !', iconURL: 'https://github.com/jellyfin/jellyfin-ux/blob/master/branding/tizen/icon.png?raw=true', url: 'https://discord.js.org' })
+	.setThumbnail('https://github.com/jellyfin/jellyfin-ux/blob/master/branding/tizen/icon.png?raw=true')
+	// .addFields(
+	// 	{ name: 'Regular field title', value: 'Some value here' },
+	// 	{ name: '\u200B', value: '\u200B' },
+	// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// )
+	.setImage('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGwyYm51aG84NWhqNHgzZjBxcGw3dnpub3owbm1wNTE1ODFoODkzZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8nmb8m82jbLfa/giphy.gif')
+	.setTimestamp()
+
+
+
 async function pingServer(url) {
   try {
     const response = await axios.get(url, { timeout: 800 });
+   // const attachment = new MessageAttachment('https://media.giphy.com/media/WpaVhEcp3Qo2TjwyI1/giphy.gif');
     if (response.status === 200) {
       if (isDown) {
         console.log(`Server at ${url} is up and running.`);
-        client.channels.cache.get(channel_status_id).send(`Server at ${url} is up and running.`)
+        client.channels.cache.get(channel_status_id).send({embeds: [serverUPEmbeded]});
       }
       isDown = false;
 
     } else {
       if (!isDown) {
         console.log(`Server at ${url} returned an unexpected status code: ${response.status}`);
-        client.channels.cache.get(channel_status_id).send(`Server at ${url} returned an unexpected status code: ${response.status}`)
+        client.channels.cache.get(channel_status_id).send({embeds: [serverDownEmbeded]});
       }
       isDown = true;
     }
   } catch (error) {
     if (!isDown) {
       console.log(`Server at ${url} is down or unreachable. Error: ${error.message}`);
-      client.channels.cache.get(channel_status_id).send(`Server at ${url} is down or unreachable. Error: ${error.message}`)
+      client.channels.cache.get(channel_status_id).send({embeds: [serverDownEmbeded]});
       isDown = true;
     }
   }
