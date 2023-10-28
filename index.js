a82f57d79dd3050f390b42501e614a554ca4b0c3
Debian <freebox@freebox-vm-14934>
2023-10-28 02:00:50 +0000

    [add] basic things to ping a serve à 2min interval and show if is down or not

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-require('dotenv').config(); //initialize dotenv
-const Discord = require('discord.js'); //import discord.js
+require("dotenv").config();
+const axios = require('axios');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
-const client = new Discord.Client(); //create new client
+const channel_status_id = process.env.CHANEL_ID;
+const serverUrl = process.env.URL_TO_PING;
+let isDown = true;
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, // give acces to guilds
+    GatewayIntentBits.GuildMessages, // give acces to messages on the guilds
+    GatewayIntentBits.MessageContent // give acces to message content on the guilds
+  ],
+  partials: [
+    Partials.Channel // give acces to channel
+  ]
 });
 
-//make sure this line is the last line
-client.login(process.env.CLIENT_TOKEN); //login bot using token
+async function pingServer(url) {
+  try {
+    const response = await axios.get(url, { timeout: 800 });
+    if (response.status === 200) {
+      if (isDown) {
+        console.log(`Server at ${url} is up and running.`);
+        client.channels.cache.get(channel_status_id).send(`Server at ${url} is up and running.`)
+      }
+      isDown = false;
+
+    } else {
+      if (!isDown) {
+        console.log(`Server at ${url} returned an unexpected status code: ${response.status}`);
+        client.channels.cache.get(channel_status_id).send(`Server at ${url} returned an unexpected status code: ${response.status}`)
+      }
+      isDown = true;
+    }
+  } catch (error) {
+    if (!isDown) {
+      console.log(`Server at ${url} is down or unreachable. Error: ${error.message}`);
+      client.channels.cache.get(channel_status_id).send(`Server at ${url} is down or unreachable. Error: ${error.message}`)
+      isDown = true;
+    }
+  }
+}
+
+client.on("ready", () => {
+  console.log("I am ready!");
+  client.user.setPresence({status: 'online', activities: [{name: 'To ping server'}]});
+  pingServer(serverUrl);
+});
+
+setInterval(() => {
+  pingServer(serverUrl);
+}, 120000);
+
+client.on("messageCreate", (message) => {
+  if (message.content.startsWith("help")) {
+    message.channel.send("en cours de dev");
+  }
+});
+
+
+client.login(process.env.CLIENT_TOKEN);
